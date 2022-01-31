3b3b33107cea5dd330c4e13d9c4b97d567b08600
SOOMIN <ananan199568@gmail.com>
2022-01-31 09:47:37 +0900

    style : ul, script : li -> ul

--- script.js
+++ script.js
@@ -9,15 +9,16 @@ typeButton.addEventListener("click", write);
 
 //write function
 function write(){
-    /* Why moved inputText into write funciton?
-    Because, inputText is null, since the user hasn't written anything on the
+    /* Why moved inputText into the write funciton?
+    Because, inputText is null, since the user hasn't written anything in the
     input bar. The brower cannot call the null variable.
      */
 
     //create nodes
     var inputText = document.querySelector("input").value;
     var result = document.getElementsByClassName("result")[0];
-    var newList = document.createElement("li");
+    var newList = document.createElement("ul");
+    /* "ul", not "li" : because it's compatible with styling (style.css) */
     var newListContent = document.createTextNode(inputText);
     
     //append nodes
