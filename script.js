c02d7ba5f78ae34ce6f644aa4c6afa1b1c4243d1
SOOMIN <ananan199568@gmail.com>
2022-01-31 09:14:47 +0900

    complete, explanation added

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-
-var typeButton = document.querySelector('button');
 // inputText, typeButton 지정은 해결
+// inputText 를 write 안으로 옮김
+var typeButton = document.querySelector('button');
 
 
-// function call(){
-//     document.getElementById("typingBox").innerText = inputText;
-// }
-
-// call();
-typeButton.addEventListener("click", write);
 //typeButton click event 
+typeButton.addEventListener("click", write);
 
 
+//write function
 function write(){
+    /* Why moved inputText into write funciton?
+    Because, inputText is null, since the user hasn't written anything on the
+    input bar. The brower cannot call the null variable.
+     */
+
+    //create nodes
     var inputText = document.querySelector("input").value;
     var result = document.getElementsByClassName("result")[0];
     var newList = document.createElement("li");
     var newListContent = document.createTextNode(inputText);
-    //create nodes
-
+    
+    //append nodes
     newList.appendChild(newListContent);
     result.appendChild(newList);
-    //append nodes
+    
 }
 
-function blue(){
-    alert("hello");
-}
+
