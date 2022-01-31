b05ed52309a93694c44b7c7c86823b3b3525983b
SOOMIN <ananan199568@gmail.com>
2022-01-31 09:35:07 +0900

    test 1

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
-// inputText, typeButton 지정은 해결
-// inputText 를 write 안으로 옮김
-var typeButton = document.querySelector('button');
+var textButton = document.getElementsByClassName("typing")[0].querySelector("button");
 
+textButton.addEventListener("click", write);
 
-//typeButton click event 
-typeButton.addEventListener("click", write);
-
-
-//write function
 function write(){
-    /* Why moved inputText into write funciton?
-    Because, inputText is null, since the user hasn't written anything on the
-    input bar. The brower cannot call the null variable.
-     */
-
-    //create nodes
-    var inputText = document.querySelector("input").value;
+    var inputText = document.getElementsByClassName("typing")[0].querySelector("input").value;
     var result = document.getElementsByClassName("result")[0];
-    var newList = document.createElement("li");
-    var newListContent = document.createTextNode(inputText);
-    
-    //append nodes
-    newList.appendChild(newListContent);
-    result.appendChild(newList);
-    
+    var list = document.createElement("li");
+    var listText = document.createTextNode(inputText);
+
+    list.appendChild(listText);
+    result.appendChild(list);
 }
-
-
