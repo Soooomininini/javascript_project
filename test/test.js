5df4f702997a7c54ee018832885076045cdee175
SOOMIN <ananan199568@gmail.com>
2022-03-07 08:41:13 +0900

    test page created

---
+++ test/test.js
@@ -0,0 +1,13 @@
+function inputData(){
+    var expense={};
+    var inputValue = document.querySelector("input").value;
+    var button = document.querySelector("button");
+    button.addEventListener(){
+        
+    }    
+
+}
+
+
+
+inputData()
