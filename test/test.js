2a12e11375675e41ba70274ee2fc4d9499d67dcd
SOOMIN <ananan199568@gmail.com>
2022-03-08 07:27:30 +0900

    test - button click event

--- test/test.js
+++ test/test.js
@@ -2,12 +2,13 @@ function inputData(){
     var expense={};
     var inputValue = document.querySelector("input").value;
     var button = document.querySelector("button");
-    button.addEventListener(){
-        
-    }    
+    button.addEventListener("click", clickEvent);
+    function clickEvent(){
+        alert(inputValue);
+    }
 
 }
 
 
 
-inputData()
+inputData();
