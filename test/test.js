efbc59e7bc670dba7c2f78ef1e540bf5cc6a2629
SOOMIN <ananan199568@gmail.com>
2022-03-10 21:46:28 +0900

    var inputValue inside clickEvent() -> valid

--- test/test.js
+++ test/test.js
@@ -1,14 +1,16 @@
 function inputData(){
     var expense={};
-    var inputValue = document.querySelector("input").value;
+    
     var button = document.querySelector("button");
     button.addEventListener("click", clickEvent);
+    var inputValue = document.querySelector("input").value;
     function clickEvent(){
+        var inputValue = document.querySelector("input").value;
         alert(inputValue);
     }
 
 }
 
 
-
+var inputValue = document.querySelector("input").value;
 inputData();
