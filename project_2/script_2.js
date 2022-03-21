3ad6115018a713e449f519d4b1927075eb4013a4
SOOMIN <ananan199568@gmail.com>
2022-03-21 21:43:52 +0900

    html table, script setTable()

--- project_2/script_2.js
+++ project_2/script_2.js
@@ -1,11 +1,13 @@
 var button = document.querySelector("button");
 var counting = 0;
+var tbody = document.querySelector("tbody");
 button.addEventListener("click", submitData(counting));
+
 
 function submitData(counting){
     if (counting==0){
-        var table = document.querySelector("table");
-        table.deleteRow(0);
+        
+        tbody.deleteRow(0);
         setTable();
         counting++;
     }
@@ -14,35 +16,38 @@ function submitData(counting){
         counting++;
     }
 
-    function setTable(){
-        //pointing vars ( name, date, amount, button)
-        var name = document.querySelectorAll("label")[0].value;
-        var date = document.querySelectorAll("label")[1].value;
-        var amount = document.querySelectorAll("label")[2].value;
-        var newButton = document.createElement("button");
+}
 
-        //creating outer dish for the pointed vars (tr, td, textNode)
-        var newRow = document.createElement("tr");
-        var newData = document.createElement("td");
-        var newText1 = document.createTextNode(name);
-        var newText2= document.createTextNode(date);
-        var newText3 = document.createTextNode(amount);
+function setTable(){
+    //pointing vars ( name, date, amount, button)
+    var name = document.querySelectorAll("label")[0].value;
+    var date = document.querySelectorAll("label")[1].value;
+    var amount = document.querySelectorAll("label")[2].value;
+    var newButton = document.createElement("button");
 
-        //putting outer dishes + vars together
-        newData.appendChild(newText1);
-        newData.appendChild(newText2);
-        newData.appendChild(newText3);
-        newData.appendChild(newButton);
+    //creating outer dish for the pointed vars (tr, td, textNode)
+    var newRow = document.createElement("tr");
+    var newData = document.createElement("td");
+    var newText1 = document.createTextNode(name);
+    var newText2= document.createTextNode(date);
+    var newText3 = document.createTextNode(amount);
 
-        //tr + td (appendChild)
-        newRow.appendChild(newData);
-        
-        //newbutton addeventlistener
-        newButton.addEventListener("click", removeRow(counting));
-        function removeRow(counting){
-            table.deleteRow(counting);
-        }
+    //putting outer dishes + vars together
+    newData.appendChild(newText1);
+    newData.appendChild(newText2);
+    newData.appendChild(newText3);
+    newData.appendChild(newButton);
 
-    }
+    //tr + td (appendChild)
+    newRow.appendChild(newData);
+    tbody.appendChild(newRow);
+    
+}
+
+//newbutton addeventlistener
+var newButton = document.querySelector("table").rows[counting+1].querySelector("button");
+newButton.addEventListener("click", removeRow(counting));
+function removeRow(counting){
+    tbody.deleteRow(counting);
 }
 
