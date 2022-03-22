0afbf14727a2fe314e1ceafe30aa4f5d36634235
SOOMIN <ananan199568@gmail.com>
2022-03-22 23:24:59 +0900

    submit button eventListener works, undefined works

--- project_2/script_2.js
+++ project_2/script_2.js
@@ -1,9 +1,9 @@
 var button = document.querySelector("button");
 var counting = 0;
 var tbody = document.querySelector("tbody");
-button.addEventListener("click", submitData(counting));
+button.addEventListener("click", submitData);
 
-function submitData(counting){
+function submitData(){
     if (counting==0){
         
         tbody.deleteRow(0);
@@ -19,10 +19,10 @@ function submitData(counting){
 
 function setTable(){
     //pointing vars ( name, date, amount, button)
-    var name = document.querySelectorAll("label")[0].value;
-    var date = document.querySelectorAll("label")[1].value;
-    var amount = document.querySelectorAll("label")[2].value;
-    var set = {name, date, amount};
+    var name = document.querySelectorAll("input")[0].value;
+    var date = document.querySelectorAll("input")[1].value;
+    var amount = document.querySelectorAll("input")[2].value;
+    var set = [name, date, amount];
     //adding a row (name, date, amount, newButton)
     var newRow = document.createElement("tr");
     for (var x=0; x<3; x++){
@@ -39,12 +39,10 @@ function setTable(){
     //attaching a button
     var newButton = document.createElement("button");
     newRow.appendChild(newButton);
+    //newButton addeventListener
+    newButton.addEventListener("click", function(){
+        newRow.deleteRow();});
+
 }
 
-//newbutton addeventlistener
-// var newButton = document.querySelector("table").rows[counting+1].querySelector("button");
-// newButton.addEventListener("click", removeRow(counting));
-// function removeRow(counting){
-//     tbody.deleteRow(counting);
 
-// }
