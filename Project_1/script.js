ed5104222d33009b96d058d0d51a7f9cc0ddb137
SOOMIN <ananan199568@gmail.com>
2022-04-14 00:03:23 +0900

    test - script.js into main

--- Project_1/script.js
+++ Project_1/script.js
@@ -3,24 +3,6 @@ var textButton = document.getElementsByClassName("typing")[0].querySelector("but
 textButton.addEventListener("click", write);
 
 function write(){
-    /* Why moved inputText into the write funciton?
-    Because, inputText is null, since the user hasn't written anything in the
-    input bar. The brower cannot call the null variable.
-     */
-
-    //create nodes
-    var inputText = document.querySelector("input").value;
-    var result = document.getElementsByClassName("result")[0];
-    var newList = document.createElement("ul");
-    /* "ul", not "li" : because it's compatible with styling (style.css) */
-    var newListContent = document.createTextNode(inputText);
-    
-    //append nodes
-    newList.appendChild(newListContent);
-    result.appendChild(newList);
-    
-}
-
     var inputText = document.getElementsByClassName("typing")[0].querySelector("input").value;
     var result = document.getElementsByClassName("result")[0];
     var list = document.createElement("li");
@@ -28,4 +10,4 @@ function write(){
 
     list.appendChild(listText);
     result.appendChild(list);
-
+}
